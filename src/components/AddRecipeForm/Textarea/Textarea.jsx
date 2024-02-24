77bad035bef28f5739bbc1a707b42bce7b5836f7
BartoszZygmunt <it@bartoszzygmunt.pl>
2024-02-24 22:50:05 +0100

    add repear

--- src/components/AddRecipeForm/Textarea/Textarea.jsx
+++ src/components/AddRecipeForm/Textarea/Textarea.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
+// import React from 'react';
+// import { useForm } from 'react-hook-form';
+
+// function TextInputw() {
+//   const { register, handleSubmit } = useForm();
+
+//   const onSubmit = data => console.log(data);
+
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <input name="example" ref={register} />
+//       <input type="submit" />
+//     </form>
+//   );
+// }
+
+// export default TextInputw;
+
+import React, { useState } from 'react';
 
 function TextInputw() {
-  const { register, handleSubmit } = useForm();
+  const [inputValue, setInputValue] = useState('');
 
-  const onSubmit = data => console.log(data);
+  const onSubmit = event => {
+    event.preventDefault();
+    console.log(inputValue);
+  };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input name="example" ref={register} />
+    <form onSubmit={onSubmit}>
+      <input
+        name="example"
+        value={inputValue}
+        onChange={e => setInputValue(e.target.value)}
+      />
       <input type="submit" />
     </form>
   );
